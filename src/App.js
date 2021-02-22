7f4a7f74bab2968c2519d4b58ba524158183a65d
mahibulmasud <mahibulmasud@gmail.com>
2021-02-23 01:25:13 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Person name="Masudur Rahman" id="181-15-1743"></Person>
+          <Person name="Jannatul" id="181-15-1744"></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props){
+  var stylefordiv={
+    border:'2px solid green',
+    margin:'10px',
+    width:'600px',
+
+  }
+  return(
+    <div style={stylefordiv}>
+      <h1><span style={{color:'green'}}>Name: {props.name}</span> </h1>
+      <h2><span style={{color:'green'}}>ID: {props.id}</span> </h2>
     </div>
   );
 }
